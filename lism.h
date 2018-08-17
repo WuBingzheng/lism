0e2dd6686151389eff0e909b84a7972261fafc81
wub <wubingzheng@gmail.com>
2018-08-17 12:15:59 +0800

    growing

--- lism.h
+++ lism.h
@@ -2,20 +2,19 @@
 #define LISM_H
 
 typedef struct {
-	const char *start;
-	const char *end;
-	const char *p;
+	const char	*p;
+	const char	*end;
+	char		last;
 } lism_ctx_t;
 
-#define LISM_OK		0
 #define LISM_ERROR	-1
 #define LISM_AGAIN	-2
 
 static inline void lism_init(lism_ctx_t *ctx, const char *str, int len)
 {
-	ctx->start = str;
-	ctx->end = str + len;
 	ctx->p = str;
+	ctx->end = str + len;
+	ctx->last = '\0';
 }
 
 static inline void lism_extend(lism_ctx_t *ctx, int len)
@@ -23,19 +22,26 @@ static inline void lism_extend(lism_ctx_t *ctx, int len)
 	ctx->end += len;
 }
 
-#define LISM_COMMA_OK		0
-#define LISM_COMMA_MORE		1
-#define LISM_COMMA_NOMORE	2
-static inline int _lism_last_tail_comma(lism_ctx_t *ctx)
+#define LISM_MORE_OK	0
+#define LISM_MORE_YES	1
+#define LISM_MORE_NO	2
+static inline int _lism_more(lism_ctx_t *ctx)
 {
-	if (ctx->p <= ctx->start || ctx->p[-1] != ')') {
-		return LISM_COMMA_OK;
-	}
-	if (ctx->p[0] == ',') {
-		ctx->p++;
-		return LISM_COMMA_MORE;
-	} else {
-		return LISM_COMMA_NOMORE;
+	switch (ctx->last) {
+	case '(':
+		return LISM_MORE_OK;
+	case ')':
+		if (ctx->p == ctx->end) {
+			return LISM_MORE_OK;
+		}
+		if (ctx->p[0] == ',') {
+			ctx->p++;
+			return LISM_MORE_YES;
+		} else {
+			return LISM_MORE_NO;
+		}
+	default: /* '\0', '=', ',' */
+		return LISM_MORE_YES;
 	}
 }
 
@@ -44,54 +50,68 @@ static inline int _lism_skip_white(lism_ctx_t *ctx)
 	while (ctx->p < ctx->end) {
 		char c = ctx->p[0];
 		if (c != ' ' && c != '\t' && c != '\n') {
-			return LISM_OK;
+			return 0;
 		}
 		ctx->p++;
 	}
 	return LISM_AGAIN;
 }
 
-static inline int lism_list_start(lism_ctx_t *ctx)
+static inline int lism_list_open(lism_ctx_t *ctx)
 {
-	if (_lism_last_tail_comma(ctx) == LISM_COMMA_NOMORE) {
-		return LISM_ERROR;
-	}
+	int more = _lism_more(ctx);
 
 	if (_lism_skip_white(ctx) == LISM_AGAIN) {
 		return LISM_AGAIN;
 	}
 
-	return *ctx->p++ == '(' ? LISM_OK : LISM_ERROR;
+	if (ctx->p[0] == '(') {
+		if (more == LISM_MORE_NO) {
+			return LISM_ERROR;
+		}
+		ctx->p++;
+		ctx->last = '(';
+		return 1;
+	} else if (ctx->p[0] == ')') {
+		if (more == LISM_MORE_YES) {
+			return LISM_ERROR;
+		}
+		ctx->last = ')';
+		return 0;
+	}
+	return LISM_ERROR;
 }
 
-static inline int lism_list_end(lism_ctx_t *ctx)
+static inline int lism_list_close(lism_ctx_t *ctx)
 {
+	if (_lism_more(ctx) == LISM_MORE_YES) {
+		return LISM_ERROR;
+	}
 	if (_lism_skip_white(ctx) == LISM_AGAIN) {
 		return LISM_AGAIN;
 	}
+	if (ctx->p[0] != ')') {
+		return LISM_ERROR;
+	}
+	ctx->p++;
+	ctx->last = ')';
 
-	return *ctx->p++ == ')' ? LISM_OK : LISM_ERROR;
+	_lism_skip_white(ctx);
+	return 1;
 }
 
 static inline int _lism_get(lism_ctx_t *ctx, char end, char invalid,
 		const char **item)
 {
-	int comma = _lism_last_tail_comma(ctx);
+	int more = _lism_more(ctx);
 
 	if (_lism_skip_white(ctx) == LISM_AGAIN) {
 		return LISM_AGAIN;
 	}
 
 	*item = ctx->p;
-	int len = -1;
 	while (ctx->p < ctx->end) {
 		char c = *ctx->p;
-		if (c == '\0') {
-			return LISM_ERROR;
-		}
-		if (c == ' ' || c == '\t' || c == '\n') {
-			return LISM_ERROR;
-		}
 		if (c == '(') {
 			return LISM_ERROR;
 		}
@@ -102,26 +122,26 @@ static inline int _lism_get(lism_ctx_t *ctx, char end, char invalid,
 			if (ctx->p == *item) {
 				return LISM_ERROR;
 			}
-			if (comma == LISM_COMMA_NOMORE) {
+			if (more == LISM_MORE_NO) {
 				return LISM_ERROR;
 			}
+			ctx->last = end;
 			return ctx->p++ - *item;
 		}
 		if (c == ')') {
 			int len = ctx->p - *item;
-			if (end == ',') { /* element */
-				if (len == 0 || comma == LISM_COMMA_NOMORE) {
-					return LISM_ERROR;
-				}
-			} else if (comma != LISM_COMMA_OK) { /* option */
-				if ((len == 0) ^ (comma == LISM_COMMA_NOMORE)) {
-					return LISM_ERROR;
-				}
+			if (more != LISM_MORE_OK && ((len == 0) ^ (more == LISM_MORE_NO))) {
+				return LISM_ERROR;
 			}
+			ctx->last = ')';
 			return len;
+		}
+		if (c < 0x21 || c == 0x7F) { /* printable char */
+			return LISM_ERROR;
 		}
 		ctx->p++;
 	}
+	ctx->p = *item;
 	return LISM_AGAIN;
 }
 
